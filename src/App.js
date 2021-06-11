f436a7f80c70756277dcfcb866343a13de8a4279
Marina8989 <marina.mill209@gmail.com>
2021-06-10 18:46:16 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import './index.css';
 
 let API_KEY = 'http://api.icndb.com/jokes/random';
 
@@ -16,9 +17,9 @@ function App() {
     }, [])
 
     return (
-        <div>
+        <div className="jokeModal">
            <h3>Chuck Norris Jokes Generator</h3>
-           <p>{joke}</p>
+           <p dangerouslySetInnerHTML = {{__html: joke}}></p>
            <button onClick={handleClick}>Next joke</button>
         </div>
     )
