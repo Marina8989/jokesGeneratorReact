b4d907a59e0d2c380ca0f42dfbf91524055a9b41
Marina8989 <marina.mill209@gmail.com>
2021-06-10 18:20:02 -0700

    react joke generator

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+
+let API_KEY = 'http://api.icndb.com/jokes/random';
 
 function App() {
+    const [joke, setJoke] = useState('');
+
+    const handleClick = () => {
+       fetch(API_KEY)
+         .then(res => res.json())
+         .then(data => setJoke(data.value.joke));
+    }
+
+    useEffect(() => {
+       handleClick();
+    }, [])
+
     return (
-        <div>Hello</div>
+        <div>
+           <h3>Chuck Norris Jokes Generator</h3>
+           <p>{joke}</p>
+           <button onClick={handleClick}>Next joke</button>
+        </div>
     )
 }
 
